364c7b8636fc2439a2cda12c3987e859124dc2e7
Мясное котясо <nadya.kott@gmail.com>
2017-09-26 21:50:25 +0500

    commit 1

--- js/main-index.js
+++ js/main-index.js
@@ -1,42 +1,97 @@
 $(document).ready(function () {
-    var items =
+    var items = [
         {
             "link": "https://csgopolygon.com",
             "name": "CSGOPOLYGON",
-            "code": "MYASKOTYAS"
-        };
-    // $(function () {
-    //
-    //     for (var i = 1; i <= 5; i++) {
-    //         var myCol = $('<div class="col-sm-3 col-md-3 pb-2"></div>');
-    //         var myPanel = $('<div class="card" > <div class="card-block"></div> </div>');
-    //         myPanel.appendTo(myCol);
-    //         myCol.appendTo('#contentPanel');
-    //         $('.container').append('<p>',{text:'Hello'}).css({background:'red'})
-    //     }
-    //     return false;
-    // })
-    $(function () {
-        for (i = 0; i < 6; i++) {
-            // var obj=$('<div>', {class: 'col-md-6'})
-            //     .append('<div>', {class: 'card'})
-            //     .append('<div>', {class: 'card-block'})
-            //     .append('<div>', {class: 'card-title'})
-            //     .append('<p>', {value: items.name});
-            // obj.appendTo('#contentPanel');
+            "code": "MYASKOTYAS",
+            "free": "0.5$"
+        },
+        {
+            "link": "https://csgofast.ru/#r/23rvjn",
+            "name": "CSGOFAST",
+            "code": "23RVJN",
+            "free": "2.5$"
+        },
+        {
+            "link": "https://www.holyboost.com/get-free/MYASKOTYAS",
+            "name": "HOLY BOOST",
+            "code": "MYASKOTYAS",
+            "free": "0.5$"
+        },
+        {
+            "link": "https://www.csgoexclusive.com",
+            "name": "CSGOEXCLUSIVE",
+            "code": "MYASKOTYAS",
+            "free": "0.5$"
+        },
+        {
+            "link": "https://csgo-case.com/",
+            "name": "CSGOCASE",
+            "code": "MYASKOTYAS",
+            "free": "0.15$"
+        },
+        {
+            "link": "https://www.greenhunt.gg/promo-code/1190123",
+            "name": "CSGOCASE",
+            "code": "1190123",
+            "free": "0.5$"
+        },
+        {
+            "link": "http://csgobounty.com/#/r/myaskotyas",
+            "name": "CSGO BOUNTY",
+            "code": "myaskotyas",
+            "free": "0.5$"
+        },
+        {
+            "link": "http://csgoroll.com/promo/76561198087537523",
+            "name": "CSGO ROLL",
+            "code": "76561198087537523",
+            "free": "0.5$"
+        },
+        {
+            "link": "https://csgoc4.com/r/myaskotyas",
+            "name": "DRAKE WING",
+            "code": "myaskotyas",
+            "free": "1$"
+        },
+        {
+            "link": "https://csgoatse.com",
+            "name": "CSGO500",
+            "code": "myaskotyas",
+            "free": "1$"
+        },
+        {
+            "link": "https://csgo500.com/",
+            "name": "CSGOatse",
+            "code": "myaskotyas",
+            "free": "1$"
+        },
+        {
+            "link": "https://csgomassive.com",
+            "name": "CSGO MASSIVE",
+            "code": "JKA81W",
+            "free": "0.5$"
+        }
 
-            var myCol = $('<div class="col-sm-3 col-md-4"></div>');
-            var myPanel = '<div class="card">'+
-                '<div class="card-block">'+
-                '<h4 class="card-title text-center">';
-                myPanel+='<a class="text-display 2" href="';
-            myPanel+=items.link+'">'+items.name+'</a>'+
-                '</h4>'+
+    ];
+    $(function () {
+        for (i = 0; i < items.length; i++) {
+            var myCol = $('<div class="col-sm-3 col-md-3"></div>');
+            var myPanel = '<div class="card">' +
+                '<div class="card-block">' +
+                '<h4 class="card-title">';
+            myPanel += '<a class="btn btn-warning mt-5" href="';
+            myPanel += items[i].link + '">' + items[i].name + '</a>' +
+                '</h4>' +
+                '<div class="card-body text-dark">';
+            myPanel += 'CODE:' + items[i].code +
+                '<p>FREE:' + items[i].free + '</p>' +
+                '</div>' +
                 '</div>';
             myCol.html(myPanel);
             myCol.appendTo('#contentPanel');
-
         }
         return false;
-    })
+    });
+
 });
